8ba6fbe8cb80b3bc8c1b095c89610b794ee29a85
Flaviane de Magalhaes <98617227+FlavianeMagalhaes@users.noreply.github.com>
2025-06-04 17:14:49 -0300

    Update Script_exclusao_segura_em_colecaoMongoDB.js

--- Script_exclusao_segura_em_colecaoMongoDB.js
+++ Script_exclusao_segura_em_colecaoMongoDB.js
@@ -12,23 +12,23 @@ try {
   session.startTransaction();
 
   const dbReal = db.getSiblingDB("SEUBANCO_MONGODB"); // SEU BANCO MONGO DB AQUI
-  const countBefore = dbReal.SUACOLECAOXYZ.countDocuments({ status: { $in: ["DONE", "ERROR"] } });
+  const countBefore = dbReal.SUACOLECAOXYZ.countDocuments({ status: { $in: ["DONE", "ERROR"] } }); // aqui você define sua colecao do mongo, os campos e valor dos campos a serem apagados 
 
   print(`Antes da exclusão, há ${countBefore} documentos com status "DONE" ou "ERROR".`);
 
   const result = dbReal.SUACOLECAOXYZ.deleteMany({ status: { $in: ["DONE", "ERROR"] } });
 
-  if (result && result.deletedCount !== undefined) {
-    print(`Foram excluídos ${result.deletedCount} documentos.`);
-  } else {
+  if (result && result.deletedCount !== undefined) { // faz a contagem antes da exclusao
+    print(`Foram excluídos ${result.deletedCount} documentos.`); //e após a exclusao faz a contagem dos campos apagados
+  } else {         //faz tratamento de erro
     print("Erro ao tentar deletar documentos.");
   }
 
-  session.commitTransaction();
+  session.commitTransaction(); //confirma as alteracoes realizadas
 
 } catch (error) {
-  print(`Erro ocorrido: ${error.message || error}`);
-  session.abortTransaction();
+  print(`Erro ocorrido: ${error.message || error}`); 
+  session.abortTransaction();  // desfaz as alteracoes realizadas 
 } finally {
   session.endSession();
 }
